2151669e298d5f075c17dc3a9e183471ff37993e
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2021-07-13 22:43:44 +0200

    Add files via upload

--- AddSongToFavsPlaylist.sh
+++ AddSongToFavsPlaylist.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-PLAYLIST='/path/to/playlist.m3u'
+
+# Check if the argument is a valid file
+if [[ -f "$1" ]]; then
+	PLAYLIST="$1"
+else
+	echo "Not a valid playlist file"
+	echo "Provide the target playlist file as an argument"
+	exit 1
+fi
 
 # Get the current playing song. -f is the format option, %file% gives the file path
 SONG=$(mpc -f %file% current)
 
 # Check if the song is already in the playlist, if not append it to it
-grep -qxF -- "$SONG" "$PLAYLIST" || echo "$SONG" >> "$PLAYLIST"
+# grep -qxF -- "$SONG" "$PLAYLIST" || echo "$SONG" >> "$PLAYLIST"
+
+# Or just add it and remove duplicates later
+echo "$SONG" >> "$PLAYLIST"
 
 # Sort alphabetically the playlist and -o output it to the second file
 # The advantage over using > is that if its the same file (like this case) > leaves an empty file
-sort -o "$PLAYLIST" "$PLAYLIST"
+# -u to remove duplicate lines
+sort -uo "$PLAYLIST" "$PLAYLIST"
+
+echo "Added $SONG to $PLAYLIST"
 
 # One liner version of the adding only if its not in the file already
-# song=$(mpc -f %file% current); grep -qxF "$song" "/path/to/playlist.m3u" || echo "$song" >> "/path/to/playlist.m3u"
-
-# Can't remember if this next one works, but it should (?)
-# mpc -f %file% current | xargs -I % grep -qxF % "/path/to/playlist.m3u" || echo % >> "/path/to/playlist.m3u"
+#song=$(mpc -f %file% current); grep -qxF "$song" "/playlist/dir/file.m3u" || echo "$song" >> "/playlist/dir/file.m3u"
+#mpc -f %file% current | xargs -I % echo % >> "/playlist/dir/file.m3u"
